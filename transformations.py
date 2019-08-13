c39ff1aeb0d046721b8568c25f8729e663af76e1
George Vince <george.vince@hotmail.co.uk>
2019-08-13 18:26:53 +0100

    feat: added transformations for source3

--- transformations.py
+++ transformations.py
@@ -5,6 +5,8 @@ import json
 from utils import extract_names
 
 def transform_s1_df(df):
+	"""Transforms SOURCE ONE files, into files ready for staging"""
+
 	df = df[['meter_name','TIMESTAMP','VALUE']]
 	
 	#Create extracted name
@@ -21,12 +23,14 @@ def transform_s1_df(df):
 	df_out = df_out[1:]
 
 	#Convert TIMESTAMP to date
-	df_out['date'] = pd.to_datetime(df_out['TIMESTAMP'], unit='ms')
-	df_out = df_out[['site_id','region','meter_name', 'date', 'VALUE','consumption']]
+	df_out['date_time'] = pd.to_datetime(df_out['TIMESTAMP'], unit='ms')
+	df_out = df_out[['site_id','region','meter_name', 'date_time', 'VALUE','consumption']]
 
 	return(df_out)
 
 def transform_s2_df(df):
+	"""Transforms SOURCE TWO files, into files ready for staging"""
+
 	#Create extracted name
 	#SITE_ID, REGION_ID, METER_NAME, TIMESTAMP, CONSUMPTION
 	extracted_name = pd.DataFrame(df['meter_name'].map(extract_names).to_list(), index=df.index)
@@ -34,14 +38,26 @@ def transform_s2_df(df):
 	df_out.rename(columns={0:'site_id', 1:'region', 2:'meter_name', 'VALUE':'consumption'}, inplace=True)
 
 	#Convert TIMESTAMP to date
-	df_out['date'] = pd.to_datetime(df_out['TIMESTAMP'], unit='ms')
-	df_out = df_out[['site_id','region','meter_name', 'date', 'consumption']]
+	df_out['date_time'] = pd.to_datetime(df_out['TIMESTAMP'], unit='ms')
+	df_out = df_out[['site_id','region','meter_name', 'date_time', 'consumption']]
 
 	return df_out
-	
-if __name__ == "__main__":
-	source_file = "Pre-Processing/data_out/SOURCE_2/AAA013_AAA013_dba_kwh.json"
 
-	data = json.load(open(source_file))
-	df = pd.DataFrame(data["data"])
-	transform_s2_df(df)
+def transform_s3_df(df):
+	"""Transforms SOURCE THREE files, into files ready for staging"""
+
+	#Remove "unnamed" column
+	df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+	#Melt DF
+	df_out = pd.melt(df, id_vars=['Company Name', 'Date'], var_name='time', value_name='consumption')
+	
+	#Convert date and time columns to date_time
+	df_out['date_time'] = pd.to_datetime(df_out['Date']+" "+df_out['time'],format='%d/%m/%Y %H:%M')
+
+	df_out['site_id'] = df_out['Company Name']
+	df_out['region'] = df_out['Company Name'].str[:3]
+	df_out['meter_name'] = "Main_Meter"
+
+	df_out = df_out[['site_id', 'region', 'meter_name', 'date_time', 'consumption']]
+	return df_out
+
